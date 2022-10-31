7e07a1115b9ce328d28aa80fa7dc354ecf049130
bhanu-priyaa <devansh80771@gmail.com>
2022-10-31 17:40:53 +0530

    added dependecies

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import {lazy , Suspense} from 'react';
 import { BrowserRouter as Router, Route,Routes } from "react-router-dom";
+import * as ROUTES from './constants/routes';
 const Login = lazy(()=> import('./pages/loginpage'))
 
 function App(){
@@ -7,7 +8,7 @@ function App(){
     <Router>
       <Suspense fallback={<p>Loading....</p>}>
         <Routes>
-          <Route exact path="/" element={<Login/>}/>
+          <Route exact path={ROUTES.LOGIN} element={<Login/>}/>
         </Routes>
       </Suspense>
     </Router>
--- src/pages/loginpage.js
+++ src/pages/loginpage.js
@@ -1,5 +1,23 @@
+import {useContext,useState,useEffect} from "react";
+import {useHistory} from "react-router-dom";
+import FirebaseContext from '../contexts/firebase';
 
 function Loginpage() {
+  const history = useHistory();
+  const {firebase} = useContext(FirebaseContext);
+
+  const [email,setemail] = useState('');
+  const[password,setpassword] = useState('');
+
+  const[error,seterror] = useState('');
+  const isInvalid = password === "" || email === "" ;
+
+  const handleLogin = () => {
+    useEffect(() => {
+      document.title = "Login - Instagram";
+    })
+  }
+
   return (
     <div>this is the loginpage</div>
   )
