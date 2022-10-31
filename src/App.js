9e4db18d4da66c75bb2bb78c8a0a9369a19752ce
bhanu-priyaa <devansh80771@gmail.com>
2022-10-31 16:50:49 +0530

    made changes in the file

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-function App() {
-  return (
-    <div>hello world</div>
-  );
+import {lazy , Suspense} from 'react';
+import { BrowserRouter as Router, Route,Routes } from "react-router-dom";
+const Login = lazy(()=> import('./pages/loginpage'))
+
+function App(){
+  return(
+    <Router>
+      <Suspense fallback={<p>Loading....</p>}>
+        <Routes>
+          <Route exact path="/" element={<Login/>}/>
+        </Routes>
+      </Suspense>
+    </Router>
+  )
 }
 
-export default App;
+export default App
---
+++ src/pages/loginpage.js
@@ -0,0 +1,8 @@
+
+function Loginpage() {
+  return (
+    <div>this is the loginpage</div>
+  )
+}
+
+export default Loginpage
